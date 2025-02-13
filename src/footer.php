dac3420708e74d404803795e8ddf7e003ea4aab8
zyto <zyttto@gmail.comgit>
2025-02-14 01:43:51 +0300

    footer fixed

--- src/footer.php
+++ src/footer.php
@@ -1,2 +1,11 @@
+<div class="container">
+    <footer class="py-3 my-4 fixed-bottom">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+            <li class="nav-item"><a href="/" class="nav-link px-2 text-body-secondary">На главную</a></li>
+        </ul>
+        <p class="text-center text-body-secondary">© 2025 devPHP.ru</p>
+    </footer>
+</div>
+
 </body>
 </html>
